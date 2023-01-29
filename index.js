5e6777c1a5099fbd0496f2854849e06977a7231a
Ardennt <wuj22@rpi.edu>
2023-01-28 20:18:53 -0500

    postgres T_T

--- index.js
+++ index.js
@@ -10,6 +10,41 @@ node index.js
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const pool = require("./db"); 
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//ROUTES
+
+// create
+
+app.post("/items", async(req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO table_test (description) VALUES($1$) RETURNING *",
+            [description]
+        )
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// get all
+
+app.get("/items", async(req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM table_test");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
 
 // once someone accesses the home page, it will send hello world
 // req = get information from the frontend
