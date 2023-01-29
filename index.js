480fd24314a7c7b9e835473143f54a67afe1827a
jiaweiw02 <jiaweiw02@gmail.com>
2023-01-28 20:32:37 -0500

    index commit

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ node index.js
 
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "TestData",
+})
 
 // once someone accesses the home page, it will send hello world
 // req = get information from the frontend
 // res = response to be sent to the frontend
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    const sqlInsert = "INSERT INTO test(id, name, price) VALUES ('18234121', 'potato', '42');";
+    db.query(sqlInsert, (err, result) => {
+        res.send("sent!");
+    })
 })
 
 // once someone access the sales page, it will send something else
