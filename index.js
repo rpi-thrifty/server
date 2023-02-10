1d27d49d7870429e1dcbdfb587fce758278d5e1b
jiaweiw02 <jiaweiw02@gmail.com>
2023-02-10 13:53:14 -0500

    merging

--- index.js
+++ index.js
@@ -1,40 +1,96 @@
-/*
-Development section with custom scripts:
-
-to run server in development mode (constant refresh)
-nodemon index.js
-
-to run server static (no refresh)
-node index.js
-*/
-
+var pg = require('pg');
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "TestData",
+        host: "localhost",
+        user: "root",
+        password: "sobored2",
+        database: "postgres",
+        port: 3002,
 })
 
-// once someone accesses the home page, it will send hello world
-// req = get information from the frontend
-// res = response to be sent to the frontend
-app.get('/', (req, res) => {
-    const sqlInsert = "INSERT INTO test(id, name, price) VALUES ('18234121', 'potato', '42');";
-    db.query(sqlInsert, (err, result) => {
-        res.send("sent!");
-    })
-})
+var conString = "postgres://root:sobored2@localhost:3002/postgres";
 
-// once someone access the sales page, it will send something else
-app.get('/sales', (req, res) => {
-    res.send("You are at sales");
-})
+var client = new pg.Client(conString);
 
-// access localhost:3001
-app.listen(3002, () => {
-    console.log("running on port 3002");
-})
+client.connect();
+
+client.query("INSERT INTO TestData (id, name) VALUES (12, 'h')");
+client.query("INSERT INTO TestData (id, name) VALUES (14, 'g')");
+
+console.log("about to query");
+
+async function here() {
+    try{
+        await client.connect();
+        var res = await client.query("SELECT * FROM TestData");
+        res.rows.forEach(row=>{
+            console.log(row);
+        });
+        await client.end();
+    } catch {
+        return;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*
+// Development section with custom scripts:
+
+// to run server in development mode (constant refresh)
+// nodemon index.js
+
+// to run server static (no refresh)
+// node index.js
+// */
+
+// const express = require("express");
+// const app = express();
+// const mysql = require("mysql");
+
+// const db = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "sobored2",
+//     database: "postgres",
+//     port: 3002,
+// })
+
+// // once someone accesses the home page, it will send hello world
+// // req = get information from the frontend
+// // res = response to be sent to the frontend
+// app.get('/', (req, res) => {
+//     const sqlInsert = "INSERT INTO TestData(id, name) VALUES (18234121, 'h');";
+//     db.query(sqlInsert, (err, result) => {
+//         res.send("sent!");
+//     })
+// })
+
+// // once someone access the sales page, it will send something else
+// app.get('/sales', (req, res) => {
+//     res.send("You are at sales");
+// })
+
+// // access localhost:3001
+// app.listen(3002, () => {
+//     console.log("running on port 3002");
+// })
